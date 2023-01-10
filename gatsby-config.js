19a4ec4bccd6f14a31923fc179d90568d048e55a
minxd95 <newobj95@gmail.com>
2023-01-10 22:36:26 +0900

    part4 done

--- gatsby-config.js
+++ gatsby-config.js
@@ -7,5 +7,15 @@ module.exports = {
     siteUrl: `https://www.yourdomain.tld`,
     description: "haha2",
   },
-  plugins: ["gatsby-plugin-image", "gatsby-plugin-sharp"],
+  plugins: [
+    "gatsby-plugin-image",
+    "gatsby-plugin-sharp",
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: `blog`,
+        path: `${__dirname}/blog`,
+      },
+    },
+  ],
 };
--- src/pages/blog.js
+++ src/pages/blog.js
@@ -1,15 +1,30 @@
+import { graphql } from "gatsby";
 import * as React from "react";
 import Layout from "../components/layout";
 import Seo from "../components/seo";
 
-const BlogPage = () => {
+const BlogPage = ({ data }) => {
   return (
     <Layout pageTitle="My Blog Posts">
-      <p>My cool posts will go in here</p>
+      <ul>
+        {data.allFile.nodes.map((node) => (
+          <li key={node.name}>{node.name}</li>
+        ))}
+      </ul>
     </Layout>
   );
 };
 
 export const Head = () => <Seo title="My Blog Posts" />;
+
+export const query = graphql`
+  query {
+    allFile {
+      nodes {
+        name
+      }
+    }
+  }
+`;
 
 export default BlogPage;
